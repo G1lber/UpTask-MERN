fe2a74c78d9c0baa8aa538d82b9e96f64469a926
G1lber <martinezfrancogilber@gmail.com>
2025-04-01 10:12:23 -0500

    1
    Conectarse a la base de datos de mongo

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+import express from 'express'
+import conectarDB from './config/db.js';
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(4000,()=>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
