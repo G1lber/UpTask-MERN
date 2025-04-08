f0381e305f34ebce60af85782e5a81f73562f183
G1lber <martinezfrancogilber@gmail.com>
2025-04-08 17:32:22 -0500

    3
    Fin, crear proyectos, obtenerlos, editarlos y eliminarlos

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import conectarDB from './config/db.js';
 import dotenv from "dotenv";
 import usuarioRoutes from "./routes/usuarioRoutes.js"
+import proyectoRoutes from "./routes/proyectoRoultes.js"
 
 const app = express();
 //Procesar la informacion tipo Json
@@ -13,7 +14,7 @@ conectarDB();
 
 //Routing
 app.use('/api/usuarios', usuarioRoutes )
-app.use('/api/proyectos', usuarioRoutes )
+app.use('/api/proyectos', proyectoRoutes )
 
 const PORT = process.env.PORT || 4000;
 app.listen(4000,()=>{
--- backend/routes/proyectoRoultes.js
+++ backend/routes/proyectoRoultes.js
@@ -13,6 +13,22 @@ import checkAuth from '../middleware/checkAuth.js'
 
 const router = express.Router()
 
-router.get('/', checkAuth, obtenerProyecto)
+// router.get('/', checkAuth, obtenerProyecto)
+// router.post('/', checkAuth, nuevoProyecto)
+
+router
+    .route('/')
+    .get(checkAuth, obtenerProyectos)
+    .post(checkAuth, nuevoProyecto)
+
+router
+    .route('/:id')
+    .get(checkAuth, obtenerProyecto)
+    .put(checkAuth, editarProyecto)
+    .delete(checkAuth, eliminarProyecto)
+
+router.get('/tareas/:id', checkAuth, obtenerTareas)
+router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
+router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
 
 export default router
